3385cbd005a0ccb872f7852a4a29fbfcd3438959
mahendrabudda <buddamahendra05@gmail.com>
2026-05-31 21:49:07 +0530

    server and ui connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 //const express = require('express')
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import router from "./routes/auth.route.js";
 import message from "./routes/message.route.js";
 import messageRouter from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 app.use("/api/auth",router);
 
 app.use("/api/message",messageRouter);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
+    app.get("*" , (_,res)=>{
+        res.sendFile(path.join(__dirname , "../frontend/dist/index.html"))
+    })
+
+}
 
 console.log(process.env.PORT);
 app.listen(3000 , ()=>{
