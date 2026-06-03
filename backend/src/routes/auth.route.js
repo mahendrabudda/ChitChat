ba7df81fb7a63a282f5361874807270a50e18e9d
mahendrabudda <buddamahendra05@gmail.com>
2026-06-03 14:06:47 +0530

    Add Arcjet middleware and update gitignore

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,8 +2,10 @@ import express from "express";
 import { logout , login, signUp } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js";
 import { updateprofile } from "../controllers/auth.controller.js";
+import {arcjetProtection} from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection);
 
 router.post("/sign-up", signUp);
 router.post("/login", login);
@@ -12,3 +14,5 @@ router.put("/update-profile", protectRoute ,updateprofile);
 router.get("/check",protectRoute,(req,res) =>res.status(200).json(req.user));
 
 export default router;
+
+
