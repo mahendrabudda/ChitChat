6d144eb606f6000697d007b01b77375b65146233
mahendrabudda <buddamahendra05@gmail.com>
2026-06-10 18:14:36 +0530

    Fix asset import casing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import ChatsList from "../components/ChatsList";
 import ContactList from "../components/ContactList";
 import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
-import backgroundImg from "../assets/backGround.jpg";
+import backgroundImg from "../assets/BackGround.jpg";
 
 function HomePage() {
   const { selectedUser, activeTab } = useChatStore();
