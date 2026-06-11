c531f97eaa99f52e766a3916c9890406b766b873
mahendrabudda <buddamahendra05@gmail.com>
2026-06-11 12:25:07 +0530

    util.js updation

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,16 +4,14 @@ const generateToken = (userId, res) => {
   const token = jwt.sign(
     { userId },
     process.env.JWT_SECRET,
-    {
-      expiresIn: "7d",
-    }
+    { expiresIn: "7d" }
   );
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV === "production" 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return token;
